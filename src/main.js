aa704eec6c35060b373a4ebe17e15f8cda19d144
Kobzar Stanislav <kobzar_stanislav@knu.ua>
2024-09-23 21:10:28 +0300

    simple routes

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import Fastify from 'fastify'
+import { createRequire } from "module"
+
+const require = createRequire(import.meta.url);
+const products = require("./products.json");
+
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.get('/products', (req, rep) => {
+    rep.send(products)
+})
+
+fastify.get('/products/:id', (req, rep) => {
+    const id = Number.parseInt(req.params.id)
+    const product = products.find(x => x.id === id)
+    if(!product){
+        rep.code(404).send({ error: 'Product does not exist' })
+    }
+    rep.send(product)
+})
+
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' })
+})
+
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})
