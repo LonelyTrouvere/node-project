4f387fc2d5143769247ceb5a9851cec7fbf4e017
Kobzar Stanislav <kobzar_stanislav@knu.ua>
2024-10-23 09:26:30 +0300

    basics covered

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const fastify = build({
   logger: true
 })
 
-fastify.listen({ port: 3000 }, function (err, address) {
+fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
