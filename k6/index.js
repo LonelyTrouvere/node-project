a90668f14be1fb07a438313390c8a591f8fa8d0d
Kobzar Stanislav <kobzar_stanislav@knu.ua>
2024-11-25 23:09:05 +0200

    k6 main task done

---
+++ k6/index.js
@@ -0,0 +1,30 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  scenarios: {
+    constant_vu_scenario: {
+        vus: 2,
+        duration: '20s',
+        executor: 'constant-vus',
+    },
+    ramping_vu_scenario: {
+        executor: 'ramping-vus',
+        stages: [
+            { duration: '20s', target: 10 },
+            { duration: '10s', target: 0 },
+        ],
+    },
+    constant_arrival_scenario: {
+        preAllocatedVUs: 2,
+        maxVUs: 10,
+        rate: 30,
+        duration: '20s',
+        executor: 'constant-arrival-rate',
+    },
+  },
+};
+export default function () {
+  http.get('http://127.0.0.1:3000/');
+  sleep(1);
+}
